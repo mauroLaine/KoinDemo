b6cd0e0c22d0229d475ddf093361858331925075
Mauricio Laine <laine.mau@gmail.com>
2019-12-02 17:05:36 -0800

    added an android logger when koin starts

--- app/src/main/java/com/laine/mauro/koindemo/KoinDemoApplication.kt
+++ app/src/main/java/com/laine/mauro/koindemo/KoinDemoApplication.kt
@@ -3,6 +3,7 @@ package com.laine.mauro.koindemo
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidFileProperties
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class KoinDemoApplication : Application() {
@@ -12,6 +13,7 @@ class KoinDemoApplication : Application() {
         startKoin {
             androidContext(this@KoinDemoApplication)
             androidFileProperties()
+            androidLogger()
             modules(listOf(applicationModule))
         }
     }
