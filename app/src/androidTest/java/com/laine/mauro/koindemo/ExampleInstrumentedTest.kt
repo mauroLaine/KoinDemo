a57f28e9995aac0d067cf54ee4ebb84478718b6a
Mauricio Laine <laine.mau@gmail.com>
2019-12-03 11:44:35 -0800

    created an instrumented test for launching the main activity

--- app/src/androidTest/java/com/laine/mauro/koindemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/laine/mauro/koindemo/ExampleInstrumentedTest.kt
@@ -1,20 +1,32 @@
 package com.laine.mauro.koindemo
 
 import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val activity = ActivityTestRule<MainActivity>(MainActivity::class.java, false, false)
+
+    @Test
+    fun activityLaunches() {
+        activity.launchActivity(null)
+        onView(withId(R.id.currencies_rv)).check(matches(isDisplayed()))
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
